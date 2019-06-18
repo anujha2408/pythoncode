c8816d7eb6a83d061678b4b5de61f4d6debaa9e1
anujha2408 <anujha23578@gmail.com>
2019-06-18 18:32:05 +0530

    my first version of add application

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is Repo for Python Codes
+## This is Code I have Learned During Python Traning
+### Total Python Repository
---
+++ add.py
@@ -0,0 +1,8 @@
+x = int(input("Enter x : "))
+y = int(input("Enter y : "))
+z = x + y 
+print("*"*80)
+print("x = ",x)
+print("y = ",y)
+print("z = ",z)
+print("*"*80)
