c1b247a7f527c6a6d565b64d14ddf81ad0b0ff0e
anujha2408 <51956736+anujha2408@users.noreply.github.com>
2019-06-18 18:53:01 +0530

    version 3.0 added some beauty

--- add.py
+++ add.py
@@ -1,7 +1,14 @@
 x,y = map(int,input("x,y : ").split(","))
 z = x + y 
+import os
+os.system('cls')
+print("\n\n\t\tWelcome to Addition Program\n\n\n\n")
 print("*"*60)
+print("*"*60)
+print()
 print(f"x = {x}".center(60))
 print(f"y = {y}".center(60))
 print(f"z = {z}".center(60))
+print()
+print("*"*60)
 print("*"*60)
